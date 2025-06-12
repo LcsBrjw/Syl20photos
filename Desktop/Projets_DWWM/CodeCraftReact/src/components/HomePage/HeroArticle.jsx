ad0b619d3192ecef41c8df7e081bceb9ef030362
Sylvain <sylvain.feillault@gmail.com>
2025-06-12 18:54:23 +0200

    style(HeroSection) modified breakpoints for img size

--- Desktop/Projets_DWWM/CodeCraftReact/src/components/HomePage/HeroArticle.jsx
+++ Desktop/Projets_DWWM/CodeCraftReact/src/components/HomePage/HeroArticle.jsx
@@ -15,8 +15,8 @@ export function HeroArticle() {
   return (
     <div>
       <h1 className="gradient-text text-center text-7xl text-[var(--dark)]">A la une</h1>
-      <div className="flex flex-col lg:flex-row gap-10 lg:w-[900px] w-full mx-auto bg-[var(--primary)] rounded-sm py-10 px-4 md:px-10">
-        <img src={article.banner_url || ""} alt="banner" className="rounded-sm max-w-[280px] md:max-w-[400px] lg:max-w-[900px] mx-auto" />
+      <div className="flex flex-col lg:flex-row items-center gap-10 lg:w-[900px] w-full mx-auto bg-[var(--primary)] rounded-sm py-10 px-4 md:px-10">
+        <img src={article.banner_url} alt="banner"  className="rounded-sm max-w-[280px] md:max-w-[400px] mx-auto md:max-h-[200px]" />
         <div>
           <h2 className="text-2xl text-white font-medium mb-4">{article.title}</h2>
           <p className="text-white text-sm mb-3">Ecrit par : {article.user.username}</p>
