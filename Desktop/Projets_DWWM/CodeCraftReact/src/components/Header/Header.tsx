d985ff26d1abdae3c13aa17ef2085678d977aac7
Sylvain <sylvain.feillault@gmail.com>
2025-06-12 18:47:17 +0200

    fix(Header) fixing home route

--- Desktop/Projets_DWWM/CodeCraftReact/src/components/Header/Header.tsx
+++ Desktop/Projets_DWWM/CodeCraftReact/src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import {CallUser} from "./CallUser";
 import {SearchBar} from "./SearchBar";
 import { Carrousel } from "./Carrousel";
+import { Link } from "react-router";
+import {ROUTES} from "../../config/routes";
 
 export const Header = () => {
   return (
@@ -8,9 +10,9 @@ export const Header = () => {
     <header className="h-[130px] sticky top-0 flex flex-col ">
       <div className="h-[70px] flex flex-row justify-between bg-[var(--dark)] items-center gap-4 px-[10%]">
         <div className="flex flex-row items-center">  
-          <a href="/">
-            <img src="./src/assets/img/logo-dark.png" alt="" className="h-[60px]" />
-          </a>
+          <Link to={ROUTES.HOME}>
+            <img src="./public/logo-dark.png" alt="Logo" className="h-[60px]" />
+          </Link>
           <div className="hidden md:block">
               <SearchBar />
           </div>
